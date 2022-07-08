b7252ab40439b91a7bc1f5329a9d03db09760489
So <im.lfq@139.com>
2022-07-08 23:51:34 +0800

    feat: 视频全屏

---
+++ src/core/action.ts
@@ -0,0 +1,49 @@
+import { Video } from "./video";
+
+export class Action {
+  name = "";
+
+  protected safeAction(action: () => void, that: Action = this) {
+    if (!Video.media) return;
+    action.apply(that);
+  }
+}
+
+export class SwitchAction extends Action {
+  get isEnable(): boolean {
+    return false;
+  }
+
+  protected enableAction() {}
+  enable() {
+    this.safeAction(this.enableAction);
+  }
+
+  protected disableAction() {}
+  disable() {
+    this.safeAction(this.disableAction);
+  }
+
+  toggle() {
+    this.isEnable ? this.disable() : this.enable();
+  }
+}
+
+/**
+ * 视频全屏
+ */
+export class Fullscreen extends SwitchAction {
+  name = "视频全屏";
+
+  get isEnable(): boolean {
+    return !!document.fullscreenElement;
+  }
+
+  protected enableAction(): void {
+    Video.player?.requestFullscreen();
+  }
+
+  protected disableAction(): void {
+    document.exitFullscreen();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-console.log("Hello World!");
+import * as Action from "./core/action";
+
+document.addEventListener("keydown", (e) => {
+  switch (true) {
+    case e.code == "Enter":
+      new Action.Fullscreen().toggle();
+      break;
+  }
+});
 
 export {};
